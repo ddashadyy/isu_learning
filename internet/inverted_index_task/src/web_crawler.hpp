e115269815aeb692a572503505a2226ef95c9bd3
ddashadyy <poletaevgena75@gmail.com>
2024-11-09 21:55:49 +0800

    need to make web crawler rightly

--- internet/inverted_index_task/src/web_crawler.hpp
+++ internet/inverted_index_task/src/web_crawler.hpp
@@ -1,21 +1,30 @@
 #pragma once
 
 #include <string>
-#include <set>
+#include <unordered_set>
 #include <curl/curl.h>
-#include <pugixml.hpp>
+#include <gumbo.h>
+
+#include "inverted_index.hpp"
 
 class WebCrawler 
 {
 public:
-    WebCrawler(int maxDepth);
-    void crawl(const std::string& url, int depth);
+    WebCrawler(int maxDepth) : m_depth(maxDepth) {}
+    void crawl(const std::string& url, int startDepth);
+
+    InvertedIndex& getInvertedIndex();
 
 private:
     std::string fetchURL(const std::string& url);
-    std::set<std::string> extractLinks(const std::string& hmtl);
+    static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
+
+    std::unordered_set<std::string> extractLinks(const std::string& html);
+    void searchForLinks(GumboNode* node, std::unordered_set<std::string>& links);
+
+
     void indexHTML(const std::string& html);
 
     int m_depth;    
-
+    InvertedIndex m_inverted_index;
 };
