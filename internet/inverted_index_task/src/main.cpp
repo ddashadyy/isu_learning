648832581ab9f4e7cf96f65c113e4b6f8e0b1c4f
ddashadyy <poletaevgena75@gmail.com>
2024-12-12 13:43:36 +0800

    все тесты сделаны, готово к демонстрации

--- internet/inverted_index_task/src/main.cpp
+++ internet/inverted_index_task/src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "inverted_index.hpp"
-#include "web_crawler.hpp"
 #include <vector>
-
+#include <nlohmann/json.hpp>
+#include <fstream>
 
 template< typename T >
 std::ostream& operator << (std::ostream& os, const std::vector<T>& con)
@@ -14,20 +14,23 @@ std::ostream& operator << (std::ostream& os, const std::vector<T>& con)
     return os;
 }
 
-#include <chrono>
+using json = nlohmann::json;
 
 int main() 
 {
 	InvertedIndex ii;
 
 	auto start = std::chrono::high_resolution_clock::now();
-	ii.indexCollection("/home/gennadiy/collection_html");
+	ii.indexCollection("/home/gennadiy/collection");
 	auto end = std::chrono::high_resolution_clock::now();
+
+	ii.serialize("serialized.json");
 
 	std::chrono::duration<double> time = end - start;
 	std::cout << time.count() << std::endl;
 
 	ii.printResult("Juliet");
+
 
     return EXIT_SUCCESS;
 }
